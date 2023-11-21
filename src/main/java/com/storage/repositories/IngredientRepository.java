f82f7118d51d326716eb6cf4d90c709786dca262
afss2 <andreferreira72@gmail.com>
2023-11-21 19:47:27 -0300

    Adding findByEmail and repository annotation

--- src/main/java/com/storage/repositories/IngredientRepository.java
+++ src/main/java/com/storage/repositories/IngredientRepository.java
@@ -7,9 +7,12 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.storage.entities.Ingredient;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, UUID> {
 
     Optional<List<Ingredient>> findByUserEmail(String email);
+    Optional<Ingredient> findByName(String name);
 
 }
